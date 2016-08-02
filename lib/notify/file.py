501fbf5fe9158e0468b8b9d6b2d69504965dfddd
Abraham Toriz Cruz <awonderfulcode@gmail.com>
2016-08-02 17:46:06 -0500

    add repo_added to file notifier

--- lib/notify/file.py
+++ lib/notify/file.py
@@ -1,13 +1,26 @@
 import json
 from datetime import datetime
 from lib.settings_loader import settings
+import os
 
 """
 Send deploy messages to json files
 """
 
 def deploy_finished(deploy_data):
-    with open('%s_'%settings.NAME + datetime.now().strftime('%Y-%m-%dT%H-%M-%S.%f')+'.json', 'w') as message_file:
+    file_name = "{server_name}_{date}.json".format(
+        server_name = settings.NAME,
+        date = datetime.now().strftime('%Y-%m-%dT%H-%M-%S.%f')
+    )
+    with open(file_name, 'w') as message_file:
+        json.dump(deploy_data, message_file)
+
+def repo_added(deploy_data):
+    file_name = "{server_name}_{date}.json".format(
+        server_name = settings.NAME,
+        date = datetime.now().strftime('%Y-%m-%dT%H-%M-%S.%f')
+    )
+    with open(os.path.join(os.path.dirname(__file__), '../../', file_name), 'w') as message_file:
         json.dump(deploy_data, message_file)
 
 if __name__ == '__main__':
