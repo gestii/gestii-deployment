3f7c8ec66dd96e0ba05cd4d7682aff3db27c081e
Abraham Toriz Cruz <awonderfulcode@gmail.com>
2016-08-03 09:52:47 -0500

    file notifier now notifies to ./logs

--- lib/notify/file.py
+++ lib/notify/file.py
@@ -7,21 +7,25 @@ import os
 Send deploy messages to json files
 """
 
-def deploy_finished(deploy_data):
-    file_name = "{server_name}_{date}.json".format(
-        server_name = settings.NAME,
-        date = datetime.now().strftime('%Y-%m-%dT%H-%M-%S.%f')
+def get_file_name(action):
+    return os.path.join(
+        os.path.dirname(__file__),
+        '../../logs',
+        "{action}_{server_name}_{date}.json".format(
+            action = action,
+            server_name = settings.NAME,
+            date = datetime.now().strftime('%Y-%m-%dT%H-%M-%S.%f')
+        )
     )
-    with open(file_name, 'w') as message_file:
+
+def deploy_finished(deploy_data):
+    with open(get_file_name('deploy'), 'w') as message_file:
+        json.dump(deploy_data, message_file)
+
+def rollback_finished(deploy_data):
+    with open(get_file_name('rollback'), 'w') as message_file:
         json.dump(deploy_data, message_file)
 
 def repo_added(deploy_data):
-    file_name = "{server_name}_{date}.json".format(
-        server_name = settings.NAME,
-        date = datetime.now().strftime('%Y-%m-%dT%H-%M-%S.%f')
-    )
-    with open(os.path.join(os.path.dirname(__file__), '../../', file_name), 'w') as message_file:
+    with open(get_file_name('add'), 'w') as message_file:
         json.dump(deploy_data, message_file)
-
-if __name__ == '__main__':
-    print()
